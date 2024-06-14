e7e73a8b889afaf7a6024bd0f81628510513e6d3
ivanl <162118702+vanyaqqe@users.noreply.github.com>
2024-06-14 15:13:24 +0200

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,77 @@
+// let value = -3;
+// if (value > 0) {
+//     console.log(true)
+// }
+// else if (value <= 0) {
+//     console.log(false)
+// }
+
+// let value = "test";
+// if (value === 'test') {
+//     console.log(true)
+// } else if (value !== 'test') {
+//     console.log(false)
+// }
+
+// let value = 13;
+// if (value < 10) {
+//     console.log(value + 5)
+// }
+//     else if (value > 10) {
+//     console.log(value - 5)
+//     }
+
+// const value = prompt("Введіть число від 1 до 12");
+// let month;
+
+// switch (value) {
+//     case "1":
+//         month = "СІЧЕНЬ";
+//         alert (`${month}`)
+//         break;
+//     case "2":
+//         month = "ЛЮТИЙ ";
+//         alert (`${month}`)
+//         break;
+//     case "3":
+//         month = "БЕРЕЗЕНЬ ";
+//         alert (`${month}`)
+//         break;
+//     case "4":
+//         month = "КВІТЕНЬ ";
+//         alert (`${month}`)
+//         break;
+//     case "5":
+//         month = "ТРАВЕНЬ ";
+//         alert (`${month}`)
+//         break;
+//     case "6":
+//         month = "ЧЕРВЕНЬ ";
+//         alert (`${month}`)
+//         break;
+//     case "7":
+//         month = "ЛИПЕНЬ ";
+//         alert (`${month}`)
+//         break;
+//     case "8":
+//         month = "СЕРПЕНЬ ";
+//         alert (`${month}`)
+//         break;
+//     case "9":
+//         month = "ВЕРЕСЕНЬ ";
+//         alert (`${month}`)
+//         break;
+//     case "10":
+//         month = "ЖОВТЕНЬ";
+//         alert (`${month}`)
+//         break;
+//     case "11":
+//         month = "ЛИСТОПАД ";
+//         alert (`${month}`)
+//         break;
+//     case "12":
+//         month = "ГРУДЕНЬ ";
+//         alert (`${month}`)
+//         break;
+// }
+
